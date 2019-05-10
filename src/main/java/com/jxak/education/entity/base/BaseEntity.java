18edc25ae8c47f41ea914810785bd534b8c3ac03
Administrator <Administrator@DESKTOP-B6J6T47>
2019-05-10 11:30:48 +0800

    添加注解

--- src/main/java/com/jxak/education/entity/base/BaseEntity.java
+++ src/main/java/com/jxak/education/entity/base/BaseEntity.java
@@ -5,40 +5,58 @@ import com.baomidou.mybatisplus.annotations.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+
 /**
- * @Description:    公有字段实体
- * @Author:         liaoyuanjie
- * @CreateDate:     2019/5/9 9:51
- * @UpdateUser:     liaoyuanjie
- * @UpdateDate:     2019/5/9 9:51
- * @UpdateRemark:   修改内容
- * @Version:        1.0
+ * @ClassName:：BaseEntity 
+ * @Comment： 公共字段实体
+ * @author ：yangth  
+ * @date ：2019年5月10日 上午11:29:13 
  */
 @Data
-@MappedSuperclass//定义公有父类字段,当前类的字段会追加到所有继承当前类的子类中,并生成库表映射关系
+@MappedSuperclass
 public class BaseEntity implements Serializable {
-    @Id
+	private static final long serialVersionUID = 5010777754852182334L;
+	
+	/**
+	 * 主键
+	 */
+	@Id
     @TableId
     @GeneratedValue
-    @Column(length = 32)//mysql主键长度不能超过768,所以必须指定长度
+    @Column(length = 32)
     private String id;
+	
+	/**
+	 * 创建用户
+	 */
     @TableField("create_user")
     @Column(name = "create_user")
     private String createUser;
+    
+    /**
+     * 创建时间
+     */
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") //入参
     @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")//出参
     @TableField("create_time")
     @Column(name = "create_time")
+    
+    /**
+     * 更新时间
+     */
     private Date createTime;
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
     @TableField("update_time")
     @Column(name = "update_time")
     private Date updateTime;
+    
+    /**
+     * 更新用户
+     */
     @TableField("update_user")
     @Column(name = "update_user")
     private String updateUser;
