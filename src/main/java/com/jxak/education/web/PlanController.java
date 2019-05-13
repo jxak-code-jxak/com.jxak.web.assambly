83ec8d575f594dfdd83c1898013d37a945cbe526
lyj <lyj1213>
2019-05-13 13:28:23 +0800

    1

--- src/main/java/com/jxak/education/web/PlanController.java
+++ src/main/java/com/jxak/education/web/PlanController.java
@@ -1,11 +1,13 @@
 package com.jxak.education.web;
 
+import com.jxak.education.entity.PlanEntity;
 import com.jxak.education.service.PlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -19,8 +21,11 @@ public class PlanController {
     @GetMapping(value = "/getPlanList")
     public Map<String,Object> getPlanListPage(){
         Map<String,Object> objectMap=new HashMap<>();
-        planService.getPlanListPage();
+        List<PlanEntity> planEntities =planService.getPlanListPage();
+        objectMap.put("data",planEntities);
+        objectMap.put("count",planEntities.size());
+        objectMap.put("msg","");
+        objectMap.put("code",0);
         return objectMap;
     }
-
 }
