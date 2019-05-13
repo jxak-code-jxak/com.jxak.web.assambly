756fcef2effe4a2f7024111d5a3d26601b544198
lyj <lyj1213>
2019-05-13 12:29:57 +0800

    1

--- src/main/java/com/jxak/education/entity/Attachment.java
+++ src/main/java/com/jxak/education/entity/Attachment.java
@@ -9,35 +9,36 @@ import javax.persistence.*;
 
 
 /**
-* @ClassName:：Attachment 
+* @ClassName:：Attachment
 * @Comment： 附件实体
-* @author ：yangth  
-* @date ：2019年5月13日 上午11:21:59 
+* @author ：yangth
+* @date ：2019年5月13日 上午11:21:59
 */
 @Data
 @EqualsAndHashCode(callSuper=true)
 @Entity
+@Table(name = "attachment_info")
 @TableName("attachment_info")
 public class Attachment extends BaseEntity{
-    
+
 	private static final long serialVersionUID = 7464307588189927927L;
-	
+
 	/**
 	 * 附件名称
 	 */
 	@TableField("attr_name_")
 	private String attrName;
-	
+
 	/**
-	 *文件后缀 
+	 *文件后缀
 	 */
 	@TableField("attr_suffix_")
 	private String attrSuffix;
-	
+
 	/**
 	 * 文件全路径
 	 */
 	@TableField("full_path_")
 	private String fullPath;
-	
+
 }
