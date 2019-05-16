abcba8663bb52d11a4b1ea64debc058f8581011c
16954655@qq.com <LiaoYJ457508>
2019-05-16 09:51:47 +0800

    视图请求错误自定义

--- src/main/java/com/jxak/education/EducationApp.java
+++ src/main/java/com/jxak/education/EducationApp.java
@@ -7,8 +7,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.Properties;
@@ -72,4 +77,19 @@ public class EducationApp extends SpringBootServletInitializer implements WebMvc
 //    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
 //        return new OptimisticLockerInterceptor();
 //    }
+    /**
+     * 视图请求错误自定义
+     * @return
+     */
+    @Bean
+    @SuppressWarnings("all")
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer(){
+        return new WebServerFactoryCustomizer<ConfigurableWebServerFactory>() {
+            @Override
+            public void customize(ConfigurableWebServerFactory factory) {
+                factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,"/error/404"));
+                factory.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR,"/error/500"));
+            }
+        };
+    }
 }
